272d77716c05a62b91e4eeb32b2de097966e2e43
Anni Metsäniitty <anni.metsaniitty@outlook.com>
2026-03-09 13:30:03 +0200

    Add model and service packages

---
+++ app/src/main/java/com/annimetsaniitty/cycletracker/model/Cycle.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.annimetsaniitty.cycletracker.model;
+
+/**
+ *
+ * @author anni
+ */
+public class Cycle {
+    
+}
